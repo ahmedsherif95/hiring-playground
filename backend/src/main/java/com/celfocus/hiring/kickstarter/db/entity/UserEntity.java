ec849aead6d7c45373e0f1faf592839dc2a1398c
ahmedsherif95 <ahmed_sherif95@hotmail.com>
2024-12-16 19:54:32 +0200

    Adding the OneToOne relationship between the cart and the user as well as the roles handling (aka permissions).

--- backend/src/main/java/com/celfocus/hiring/kickstarter/db/entity/UserEntity.java
+++ backend/src/main/java/com/celfocus/hiring/kickstarter/db/entity/UserEntity.java
@@ -1,24 +1,38 @@
 package com.celfocus.hiring.kickstarter.db.entity;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 @Entity
 @Table(name = "USERS")
-public class UserEntity {
+public class UserEntity implements UserDetails {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String username;
     private String password;
-    private String permission;
 
-    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+
+    @Column(name = "roles")
+    private String roles;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_ID")
     private CartEntity cartEntity;
 
-    public UserEntity(Integer id, String username, String password, String permission) {
+
+    public UserEntity(Integer id, String username, String password, List<String> roles) {
         this.id = id;
         this.username = username;
         this.password = password;
-        this.permission = permission;
+        this.setRoles(roles);
     }
 
     public UserEntity() {}
@@ -35,8 +49,38 @@ public class UserEntity {
         return username;
     }
 
+    @Override
+    public boolean isAccountNonExpired() {
+//        return UserDetails.super.isAccountNonExpired();
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.unmodifiableList(
+                getRoles().stream()
+                        .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                        .toList()
+        );
     }
 
     public String getPassword() {
@@ -47,11 +91,12 @@ public class UserEntity {
         this.password = password;
     }
 
-    public String getPermission() {
-        return permission;
+    public List<String> getRoles() {
+        return roles != null ? Arrays.asList(roles.split(",")) : Collections.emptyList();
+
     }
 
-    public void setPermission(String permission) {
-        this.permission = permission;
+    public void setRoles(List<String> roles) {
+        this.roles = roles != null ? String.join(",", roles) : null;
     }
 }
