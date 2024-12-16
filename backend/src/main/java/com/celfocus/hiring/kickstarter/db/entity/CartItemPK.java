a964b55a56491e57ad25e95d5648aaa754b694dc
ahmedsherif95 <ahmed_sherif95@hotmail.com>
2024-12-16 22:14:49 +0200

    Overriding equals and hashCode for comparisons if needed.

--- backend/src/main/java/com/celfocus/hiring/kickstarter/db/entity/CartItemPK.java
+++ backend/src/main/java/com/celfocus/hiring/kickstarter/db/entity/CartItemPK.java
@@ -3,6 +3,7 @@ package com.celfocus.hiring.kickstarter.db.entity;
 import jakarta.persistence.Embeddable;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 @Embeddable
 public class CartItemPK implements Serializable {
@@ -32,4 +33,18 @@ public class CartItemPK implements Serializable {
     public void setItemId(String itemId) {
         this.itemId = itemId;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CartItemPK that = (CartItemPK) o;
+        return Objects.equals(cartId, that.cartId) && Objects.equals(itemId, that.itemId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cartId, itemId);
+    }
+
 }
